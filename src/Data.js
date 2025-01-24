040fcfb27b24730a5d3aa06c0b9c6cc8b5d6c3c0
sivalinga7581 <sivalinga7581@gmail.com>
2025-01-24 11:22:36 +0530

    initial

---
+++ src/Data.js
@@ -0,0 +1,76 @@
+import iphone from './images/iphone.jpg';
+import airpods from './images/airpods.jpg';
+import macbook from './images/macbook.jpg';
+import skatingshooe from './images/skatingshooe.jpg';
+import tv from './images/tv.jpg';
+import cycle from './images/cycle.webp';
+import tab from './images/tab.webp';
+import hormonium from './images/hormonium.jpg';
+import aircooler from './images/aircooler.webp'
+const Data = [
+    {
+        id:1,	
+        name:"iphone",
+        description:"This is iphone",
+        price:175000,
+        $:	8377,
+        img:iphone
+    },
+    {
+        id:2,
+        name:"airpods",
+        description:"This is airpods",
+        price:5000,
+        img:airpods
+    },
+    {
+        id:3,
+        name:"macbook",
+        description:"This is macbook",
+        price:105000,
+        img:macbook
+    },
+    {
+        id:4,
+        name:"skatingshooe",
+        description:"This is skatingshooe",
+        price:7000,
+        img:skatingshooe
+    },
+    {
+        id:5,
+        name:"tv",
+        description:"This is tv",
+        price:74000,
+        img:tv
+    },
+    {
+        id:6,
+        name:"cycle",
+        description:"This is cycle",
+        price:26000,
+        img:cycle
+    },
+    {
+        id:7,
+        name:"tab",
+        description:"This is tab",
+        price:75000,
+        img:tab
+    },
+    {
+        id:8,
+        name:"hormonium",
+        description:"This is hormonium",
+        price:85000,
+        img:hormonium
+    },
+    {
+        id:9,
+        name:"aircooler",
+        description:"This is aircooler",
+        price:18000,
+        img:aircooler
+    }
+]
+export default Data;
